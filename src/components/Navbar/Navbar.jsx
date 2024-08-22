ca7e81dd3f93db1dc7803e7f022d807437fc7dc1
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-08-23 01:22:54 +0600

    "Updated Navbar component to add login/signup buttons and user profile dropdown"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,13 @@ import {
 import React from "react";
 import { Link } from "react-router-dom";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { LogOut, User2 } from "lucide-react";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="bg-white custom-border">
+    <div className="bg-white ">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
           <h1 className="text-2xl font-bold">
@@ -28,20 +31,51 @@ const Navbar = () => {
               <Link>Browser</Link>
             </li>
           </ul>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Avatar>
-                <AvatarImage
-                  className="cursor-pointer"
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className="w-80 custom-border">
-              <h1 className="w-16 custom-border">mithu</h1>
-            </PopoverContent>
-          </Popover>
+          {!user ? (
+            <div className=" flex font-medium gap-2">
+              <Button variant="outline">Login</Button>
+              <Button variant="outline">Signup</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar>
+                  <AvatarImage
+                    className="cursor-pointer"
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80 p-2  ">
+                <div className="flex gap-3 items-center">
+                  <Avatar>
+                    <AvatarImage
+                      className="cursor-pointer space-y-2"
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h1 className="font-medium ">Iftekhar Alam</h1>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-3 text-gray-600">
+                  <div className="flex  cursor-pointer gap-2 w-fit items-center">
+                    <User2></User2>
+                    <Button variant="ghost">View Profile</Button>
+                  </div>
+                  <div className="flex cursor-pointer gap-2 w-fit items-center">
+                    <LogOut></LogOut>
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
